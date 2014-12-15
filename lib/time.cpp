f98e5f0986e4ef199ba72b879a31bd8d548dd3fa
Greg Hewgill <greg@hewgill.com>
2014-12-15 21:06:41 +1300

    add time.sleep function

--- lib/time.cpp
+++ lib/time.cpp
@@ -1,3 +1,5 @@
+#include <thread>
+
 #include "number.h"
 #include "rtl_platform.h"
 
@@ -6,6 +8,12 @@ namespace rtl {
 Number time$now()
 {
     return rtl_time_now();
+}
+
+void time$sleep(Number seconds)
+{
+    std::chrono::microseconds us(number_to_uint64(number_multiply(seconds, number_from_uint32(1000000))));
+    std::this_thread::sleep_for(us);
 }
 
 } // namespace rtl
