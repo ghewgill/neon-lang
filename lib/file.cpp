39c3580116a1b47d76046d8b63d455fef2ba07ac
Larry Frieson <larryf@mlinks.net>
2015-02-14 13:32:25 -0800

    Added binary file reader RTL function.

--- lib/file.cpp
+++ lib/file.cpp
@@ -12,11 +12,29 @@ std::vector<std::string> file$lines(const std::string &filename)
     std::vector<std::string> r;
     std::ifstream f(filename);
     if (not f.is_open()) {
-	throw RtlException("FileOpenError", filename);
+        throw RtlException("FileOpenError", filename);
     }
     std::string s;
     while (std::getline(f, s)) {
-	r.push_back(s);
+        r.push_back(s);
+    }
+    return r;
+}
+
+std::vector<Number> file$data(const std::string &filename)
+{
+    std::vector<Number> r;
+    std::ifstream f(filename, std::ios_base::binary);
+    if (not f.is_open()) {
+        throw RtlException("FileOpenError", filename);
+    }
+    f.seekg(0, f.end);
+    size_t length = f.tellg();
+    f.seekg(0, f.beg);
+    for (size_t x = 0; x < length; x++) {
+        uint8_t c;
+        f.read(reinterpret_cast<char *>(&c), sizeof(c));
+        r.push_back(number_from_uint8(c));
     }
     return r;
 }
