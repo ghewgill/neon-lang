e060706d528d78a187de5eb3d88cb5f2f78d55e5
Greg Hewgill <greg@hewgill.com>
2015-08-14 20:22:31 +1200

    Add 64-bit bitwise operations, rename 32 bit functions

--- lib/bitwise.cpp
+++ lib/bitwise.cpp
@@ -29,6 +29,30 @@ public:
 const Number traits<uint32_t>::MIN = traits<uint32_t>::to_number(std::numeric_limits<uint32_t>::min());
 const Number traits<uint32_t>::MAX = traits<uint32_t>::to_number(std::numeric_limits<uint32_t>::max());
 
+template <> class traits<int64_t> {
+public:
+    static const unsigned int BITS = 64;
+    static const Number MIN;
+    static const Number MAX;
+    static Number to_number(int64_t x) { return number_from_sint64(x); }
+    static int64_t from_number(Number x) { return number_to_sint64(x); }
+};
+
+const Number traits<int64_t>::MIN = traits<int64_t>::to_number(std::numeric_limits<int64_t>::min());
+const Number traits<int64_t>::MAX = traits<int64_t>::to_number(std::numeric_limits<int64_t>::max());
+
+template <> class traits<uint64_t> {
+public:
+    static const unsigned int BITS = 64;
+    static const Number MIN;
+    static const Number MAX;
+    static Number to_number(uint64_t x) { return number_from_uint64(x); }
+    static uint64_t from_number(Number x) { return number_to_uint64(x); }
+};
+
+const Number traits<uint64_t>::MIN = traits<uint64_t>::to_number(std::numeric_limits<uint64_t>::min());
+const Number traits<uint64_t>::MAX = traits<uint64_t>::to_number(std::numeric_limits<uint64_t>::max());
+
 template <typename T> void range_check(Number x)
 {
     if (number_is_less(x, traits<T>::MIN) || number_is_greater(x, traits<T>::MAX)) {
@@ -165,59 +189,27 @@ template <typename T> Number bitwise_xor(Number x, Number y)
 
 namespace rtl {
 
-Number bitwise$and(Number x, Number y)
-{
-    return bitwise_and<uint32_t>(x, y);
-}
-
-Number bitwise$extract(Number x, Number n, Number w)
-{
-    return bitwise_extract<uint32_t>(x, n, w);
-}
-
-bool bitwise$get(Number x, Number n)
-{
-    return bitwise_get<uint32_t>(x, n);
-}
-
-Number bitwise$not(Number x)
-{
-    return bitwise_not<uint32_t>(x);
-}
-
-Number bitwise$or(Number x, Number y)
-{
-    return bitwise_or<uint32_t>(x, y);
-}
-
-Number bitwise$replace(Number x, Number n, Number w, Number y)
-{
-    return bitwise_replace<uint32_t>(x, n, w, y);
-}
-
-Number bitwise$set(Number x, Number n, bool v)
-{
-    return bitwise_set<uint32_t>(x, n, v);
-}
-
-Number bitwise$shift_left(Number x, Number n)
-{
-    return bitwise_shift_left<uint32_t>(x, n);
-}
-
-Number bitwise$shift_right(Number x, Number n)
-{
-    return bitwise_shift_right<uint32_t>(x, n);
-}
-
-Number bitwise$shift_right_signed(Number x, Number n)
-{
-    return bitwise_shift_right_signed<int32_t>(x, n);
-}
-
-Number bitwise$xor(Number x, Number y)
-{
-    return bitwise_xor<uint32_t>(x, y);
-}
+Number bitwise$and32(Number x, Number y)                            { return bitwise_and<uint32_t>(x, y); }
+Number bitwise$and64(Number x, Number y)                            { return bitwise_and<uint64_t>(x, y); }
+Number bitwise$extract32(Number x, Number n, Number w)              { return bitwise_extract<uint32_t>(x, n, w); }
+Number bitwise$extract64(Number x, Number n, Number w)              { return bitwise_extract<uint64_t>(x, n, w); }
+bool bitwise$get32(Number x, Number n)                              { return bitwise_get<uint32_t>(x, n); }
+bool bitwise$get64(Number x, Number n)                              { return bitwise_get<uint64_t>(x, n); }
+Number bitwise$not32(Number x)                                      { return bitwise_not<uint32_t>(x); }
+Number bitwise$not64(Number x)                                      { return bitwise_not<uint64_t>(x); }
+Number bitwise$or32(Number x, Number y)                             { return bitwise_or<uint32_t>(x, y); }
+Number bitwise$or64(Number x, Number y)                             { return bitwise_or<uint64_t>(x, y); }
+Number bitwise$replace32(Number x, Number n, Number w, Number y)    { return bitwise_replace<uint32_t>(x, n, w, y); }
+Number bitwise$replace64(Number x, Number n, Number w, Number y)    { return bitwise_replace<uint64_t>(x, n, w, y); }
+Number bitwise$set32(Number x, Number n, bool v)                    { return bitwise_set<uint32_t>(x, n, v); }
+Number bitwise$set64(Number x, Number n, bool v)                    { return bitwise_set<uint64_t>(x, n, v); }
+Number bitwise$shift_left32(Number x, Number n)                     { return bitwise_shift_left<uint32_t>(x, n); }
+Number bitwise$shift_left64(Number x, Number n)                     { return bitwise_shift_left<uint64_t>(x, n); }
+Number bitwise$shift_right32(Number x, Number n)                    { return bitwise_shift_right<uint32_t>(x, n); }
+Number bitwise$shift_right64(Number x, Number n)                    { return bitwise_shift_right<uint64_t>(x, n); }
+Number bitwise$shift_right_signed32(Number x, Number n)             { return bitwise_shift_right_signed<int32_t>(x, n); }
+Number bitwise$shift_right_signed64(Number x, Number n)             { return bitwise_shift_right_signed<int64_t>(x, n); }
+Number bitwise$xor32(Number x, Number y)                            { return bitwise_xor<uint32_t>(x, y); }
+Number bitwise$xor64(Number x, Number y)                            { return bitwise_xor<uint64_t>(x, y); }
 
 } // namespace rtl
