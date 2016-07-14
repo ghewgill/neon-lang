58389ef7d909e1b1eb336d664e9ec96f1993b2bd
Greg Hewgill <greg@hewgill.com>
2016-07-15 07:45:34 +1200

    Fix perf_lexer for win32

--- tests/perf_lexer.cpp
+++ tests/perf_lexer.cpp
@@ -3,10 +3,31 @@
 #include <iostream>
 #include <sstream>
 #include <string.h>
-#include <sys/time.h>
 
 #include "lexer.h"
 #include "util.h"
+
+#ifdef _WIN32
+
+#include <windows.h>
+
+time_t get_second()
+{
+    return GetTickCount() / 1000;
+}
+
+#else
+
+#include <sys/time.h>
+
+time_t get_second()
+{
+    struct timeval tv;
+    gettimeofday(&tv, NULL);
+    return tv.tv_sec;
+}
+
+#endif
 
 int main(int, char *[])
 {
@@ -15,20 +36,19 @@ int main(int, char *[])
     ss << inf.rdbuf();
     std::string source = ss.str();
 
-    struct timeval tv;
-    gettimeofday(&tv, NULL);
-    time_t start = tv.tv_sec;
+    time_t start = get_second();
+    time_t now;
     do {
-        gettimeofday(&tv, NULL);
-    } while (tv.tv_sec == start);
+        now = get_second();
+    } while (now == start);
 
-    start = tv.tv_sec;
+    start = now;
     int count = 0;
     do {
         tokenize("", source);
         count++;
-        gettimeofday(&tv, NULL);
-    } while (tv.tv_sec - start < 1);
+        now = get_second();
+    } while (now - start < 1);
 
     printf("count %d\n", count);
 }
