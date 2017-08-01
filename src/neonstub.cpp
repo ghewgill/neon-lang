d11d83dcedf0235d62bdc3e64eb0794d345f4035
Greg Hewgill <greg@hewgill.com>
2017-08-01 14:14:40 +1200

    Use GetModuleFileName on win32 to get exe name

--- src/neonstub.cpp
+++ src/neonstub.cpp
@@ -1,12 +1,22 @@
 #include "bundle.h"
 #include "exec.h"
 
+#ifdef _WIN32
+#include <windows.h>
+#endif
+
 int main(int argc, char *argv[])
 {
     bool enable_assert = true;
     unsigned short debug_port = 0;
 
-    run_from_bundle(argv[0], enable_assert, debug_port, argc, argv);
+    const char *exe = argv[0];
+    #ifdef _WIN32
+        char buf[MAX_PATH];
+        GetModuleFileName(NULL, buf, sizeof(buf));
+        exe = buf;
+    #endif
+    run_from_bundle(exe, enable_assert, debug_port, argc, argv);
 
     return 0;
 }
