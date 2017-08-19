1af6fc48fde545137aa490a3d3acf7928352c3bd
Greg Hewgill <greg@hewgill.com>
2017-08-19 20:29:59 +1200

    Update documentation from d30e5e3

--- samples/javascript/searchdata.js
+++ samples/javascript/searchdata.js
@@ -1,5 +1,5 @@
 var indexSectionsWithContent = {
-   "Functions": {
+   "General": {
       "Symbols": false,
       "Numbers": false,
       "A": false,
@@ -7,9 +7,9 @@ var indexSectionsWithContent = {
       "C": false,
       "D": false,
       "E": false,
-      "F": false,
+      "F": true,
       "G": false,
-      "H": false,
+      "H": true,
       "I": false,
       "J": false,
       "K": false,
@@ -29,7 +29,7 @@ var indexSectionsWithContent = {
       "Y": false,
       "Z": false
       },
-   "General": {
+   "Functions": {
       "Symbols": false,
       "Numbers": false,
       "A": false,
@@ -37,9 +37,9 @@ var indexSectionsWithContent = {
       "C": false,
       "D": false,
       "E": false,
-      "F": true,
+      "F": false,
       "G": false,
-      "H": true,
+      "H": false,
       "I": false,
       "J": false,
       "K": false,
