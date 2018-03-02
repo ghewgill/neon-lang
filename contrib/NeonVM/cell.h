4b74178114a7b3c8791b51c18855f45dd2fb846d
Larry Frieson <larryf@mlinks.net>
2018-03-01 20:08:03 -0800

    fix _strdup() call under Windows, and normalized line endings.

--- contrib/NeonVM/cell.h
+++ contrib/NeonVM/cell.h
@@ -1,20 +1,6 @@
 #ifndef __CELL_H
 #define __CELL_H
 
-//#define DECIMAL_GLOBAL_ROUNDING 1
-//#define DECIMAL_GLOBAL_EXCEPTION_FLAGS 1
-//
-//#ifndef _WCHAR_T_DEFINED
-//#define _WCHAR_T_DEFINED
-//#endif
-//#include "bid_conf.h"
-//#ifdef _MSC_VER
-//#pragma warning(push, 0)
-//#endif
-//#include "bid_functions.h"
-//#ifdef _MSC_VER
-//#pragma warning(pop)
-//#endif
 #include "number.h"
 
 typedef enum tagEtype {
@@ -29,12 +15,6 @@ typedef struct tagTCell {
     struct tagTCell *address;
     char *string;
     CellType type;
-    //enum tagEtype {
-    //    None,
-    //    Address,
-    //    Number,
-    //    String,
-    //} Type;
 } Cell;
 
 Cell *cell_newCell();
@@ -44,7 +24,6 @@ Cell *cell_fromNumber(BID_UINT128 n);
 Cell *cell_fromString(const char *s);
 Cell *cell_fromCell(const Cell *c);
 Cell *cell_fromAddress(Cell *c);
-//Cell *cell_copyCell(const Cell *c);
 void cell_copyCell(Cell *dest, const Cell *source);
 void  cell_resetCell(Cell *c);
 
