9aaea832ea0f2aabeb3480661352c81c4b7bd161
Greg Hewgill <greg@hewgill.com>
2018-11-11 10:06:34 +0000

    wip

--- src/number.cpp
+++ src/number.cpp
@@ -2,6 +2,13 @@
 
 #include <iso646.h>
 
+class Initialiser {
+public:
+    Initialiser() {
+        mpfr_set_default_prec(128);
+    }
+} initialiser;
+
 Number number_add(Number x, Number y)
 {
     Number r;
@@ -46,127 +53,184 @@ Number number_pow(Number x, Number y)
 
 Number number_negate(Number x)
 {
-    return Number();//bid128_negate(x.x);
+    Number r;
+    mpfr_neg(r.x, x.x, MPFR_RNDN);
+    return r;
 }
 
 Number number_abs(Number x)
 {
-    return Number();//bid128_abs(x.x);
+    Number r;
+    mpfr_abs(r.x, x.x, MPFR_RNDN);
+    return r;
 }
 
 Number number_sign(Number x)
 {
-    return Number();//bid128_copySign(bid128_from_uint32(1), x.x);
+    Number r;
+    int s = mpfr_sgn(x.x);
+    if (s > 0) {
+        mpfr_set_si(r.x, 1, MPFR_RNDN);
+    } else if (s < 0) {
+        mpfr_set_si(r.x, -1, MPFR_RNDN);
+    } else {
+        mpfr_set_si(r.x, 0, MPFR_RNDN);
+    }
+    return r;
 }
 
 Number number_ceil(Number x)
 {
-    return Number();//bid128_round_integral_positive(x.x);
+    Number r;
+    mpfr_ceil(r.x, x.x);
+    return r;
 }
 
 Number number_floor(Number x)
 {
-    return Number();//bid128_round_integral_negative(x.x);
+    Number r;
+    mpfr_floor(r.x, x.x);
+    return r;
 }
 
 Number number_trunc(Number x)
 {
-    return Number();//bid128_round_integral_zero(x.x);
+    Number r;
+    mpfr_trunc(r.x, x.x);
+    return r;
 }
 
 Number number_exp(Number x)
 {
-    return Number();//bid128_exp(x.x);
+    Number r;
+    mpfr_exp(r.x, x.x, MPFR_RNDN);
+    return r;
 }
 
 Number number_log(Number x)
 {
-    return Number();//bid128_log(x.x);
+    Number r;
+    mpfr_log(r.x, x.x, MPFR_RNDN);
+    return r;
 }
 
 Number number_sqrt(Number x)
 {
-    return Number();//bid128_sqrt(x.x);
+    Number r;
+    mpfr_sqrt(r.x, x.x, MPFR_RNDN);
+    return r;
 }
 
 Number number_acos(Number x)
 {
-    return Number();//bid128_acos(x.x);
+    Number r;
+    mpfr_acos(r.x, x.x, MPFR_RNDN);
+    return r;
 }
 
 Number number_asin(Number x)
 {
-    return Number();//bid128_asin(x.x);
+    Number r;
+    mpfr_asin(r.x, x.x, MPFR_RNDN);
+    return r;
 }
 
 Number number_atan(Number x)
 {
-    return Number();//bid128_atan(x.x);
+    Number r;
+    mpfr_atan(r.x, x.x, MPFR_RNDN);
+    return r;
 }
 
 Number number_cos(Number x)
 {
-    return Number();//bid128_cos(x.x);
+    Number r;
+    mpfr_cos(r.x, x.x, MPFR_RNDN);
+    return r;
 }
 
 Number number_sin(Number x)
 {
-    return Number();//bid128_sin(x.x);
+    Number r;
+    mpfr_sin(r.x, x.x, MPFR_RNDN);
+    return r;
 }
 
 Number number_tan(Number x)
 {
-    return Number();//bid128_tan(x.x);
+    Number r;
+    mpfr_tan(r.x, x.x, MPFR_RNDN);
+    return r;
 }
 
 Number number_acosh(Number x)
 {
-    return Number();//bid128_acosh(x.x);
+    Number r;
+    mpfr_acosh(r.x, x.x, MPFR_RNDN);
+    return r;
 }
 
 Number number_asinh(Number x)
 {
-    return Number();//bid128_asinh(x.x);
+    Number r;
+    mpfr_asinh(r.x, x.x, MPFR_RNDN);
+    return r;
 }
 
 Number number_atanh(Number x)
 {
-    return Number();//bid128_atanh(x.x);
+    Number r;
+    mpfr_atanh(r.x, x.x, MPFR_RNDN);
+    return r;
 }
 
 Number number_atan2(Number y, Number x)
 {
-    return Number();//bid128_atan2(y.x, x.x);
+    Number r;
+    mpfr_atan2(r.x, y.x, x.x, MPFR_RNDN);
+    return r;
 }
 
 Number number_cbrt(Number x)
 {
-    return Number();//bid128_cbrt(x.x);
+    Number r;
+    mpfr_cbrt(r.x, x.x, MPFR_RNDN);
+    return r;
 }
 
 Number number_cosh(Number x)
 {
-    return Number();//bid128_cosh(x.x);
+    Number r;
+    mpfr_cosh(r.x, x.x, MPFR_RNDN);
+    return r;
 }
 
 Number number_erf(Number x)
 {
-    return Number();//bid128_erf(x.x);
+    Number r;
+    mpfr_erf(r.x, x.x, MPFR_RNDN);
+    return r;
 }
 
 Number number_erfc(Number x)
 {
-    return Number();//bid128_erfc(x.x);
+    Number r;
+    mpfr_erfc(r.x, x.x, MPFR_RNDN);
+    return r;
 }
 
 Number number_exp2(Number x)
 {
-    return Number();//bid128_exp2(x.x);
+    Number r;
+    mpfr_exp2(r.x, x.x, MPFR_RNDN);
+    return r;
 }
 
 Number number_expm1(Number x)
 {
-    return Number();//bid128_expm1(x.x);
+    Number r;
+    mpfr_expm1(r.x, x.x, MPFR_RNDN);
+    return r;
 }
 
 Number number_frexp(Number x, int *exp)
@@ -176,7 +240,9 @@ Number number_frexp(Number x, int *exp)
 
 Number number_hypot(Number x, Number y)
 {
-    return Number();//bid128_hypot(x.x, y.x);
+    Number r;
+    mpfr_hypot(r.x, x.x, y.x, MPFR_RNDN);
+    return r;
 }
 
 Number number_ldexp(Number x, int exp)
@@ -186,22 +252,31 @@ Number number_ldexp(Number x, int exp)
 
 Number number_lgamma(Number x)
 {
-    return Number();//bid128_lgamma(x.x);
+    Number r;
+    int sign;
+    mpfr_lgamma(r.x, &sign, x.x, MPFR_RNDN);
+    return r;
 }
 
 Number number_log10(Number x)
 {
-    return Number();//bid128_log10(x.x);
+    Number r;
+    mpfr_log10(r.x, x.x, MPFR_RNDN);
+    return r;
 }
 
 Number number_log1p(Number x)
 {
-    return Number();//bid128_log1p(x.x);
+    Number r;
+    mpfr_log1p(r.x, x.x, MPFR_RNDN);
+    return r;
 }
 
 Number number_log2(Number x)
 {
-    return Number();//bid128_log2(x.x);
+    Number r;
+    mpfr_log2(r.x, x.x, MPFR_RNDN);
+    return r;
 }
 
 Number number_nearbyint(Number x)
@@ -211,12 +286,16 @@ Number number_nearbyint(Number x)
 
 Number number_sinh(Number x)
 {
-    return Number();//bid128_sinh(x.x);
+    Number r;
+    mpfr_sinh(r.x, x.x, MPFR_RNDN);
+    return r;
 }
 
 Number number_tanh(Number x)
 {
-    return Number();//bid128_tanh(x.x);
+    Number r;
+    mpfr_tanh(r.x, x.x, MPFR_RNDN);
+    return r;
 }
 
 Number number_tgamma(Number x)
@@ -226,48 +305,47 @@ Number number_tgamma(Number x)
 
 bool number_is_zero(Number x)
 {
-    return false;//bid128_isZero(x.x) != 0;
+    return mpfr_zero_p(x.x);
 }
 
 bool number_is_negative(Number x)
 {
-    return false;//bid128_isSigned(x.x) != 0;
+    return mpfr_sgn(x.x) < 0;
 }
 
 bool number_is_equal(Number x, Number y)
 {
-    return false;//bid128_quiet_equal(x.x, y.x) != 0;
+    return mpfr_equal_p(x.x, y.x);
 }
 
 bool number_is_not_equal(Number x, Number y)
 {
-    return false;//bid128_quiet_not_equal(x.x, y.x) != 0;
+    return not mpfr_equal_p(x.x, y.x);
 }
 
 bool number_is_less(Number x, Number y)
 {
-    return false;//bid128_quiet_less(x.x, y.x) != 0;
+    return mpfr_less_p(x.x, y.x);
 }
 
 bool number_is_greater(Number x, Number y)
 {
-    return false;//bid128_quiet_greater(x.x, y.x) != 0;
+    return mpfr_greater_p(x.x, y.x);
 }
 
 bool number_is_less_equal(Number x, Number y)
 {
-    return false;//bid128_quiet_less_equal(x.x, y.x) != 0;
+    return mpfr_lessequal_p(x.x, y.x);
 }
 
 bool number_is_greater_equal(Number x, Number y)
 {
-    return false;//bid128_quiet_greater_equal(x.x, y.x) != 0;
+    return mpfr_greaterequal_p(x.x, y.x);
 }
 
 bool number_is_integer(Number x)
 {
-    //BID_UINT128 i = bid128_round_integral_zero(x.x);
-    return false;//bid128_quiet_equal(x.x, i) != 0;
+    return mpfr_integer_p(x.x);
 }
 
 bool number_is_odd(Number x)
@@ -277,14 +355,15 @@ bool number_is_odd(Number x)
 
 bool number_is_nan(Number x)
 {
-    return false;//bid128_isNaN(x.x) != 0;
+    return mpfr_nan_p(x.x);
 }
 
 std::string number_to_string(Number x)
 {
     mpfr_exp_t exp;
-    char buf[1+40+1];
-    mpfr_get_str(buf, &exp, 10, 40, x.x, MPFR_RNDN);
+    const int DECIMAL_DIGITS = 38;
+    char buf[1+DECIMAL_DIGITS+1];
+    mpfr_get_str(buf, &exp, 10, DECIMAL_DIGITS, x.x, MPFR_RNDN);
     std::string r = buf;
     if (not r.empty() && r[0] == '@') {
         return r;
@@ -294,62 +373,70 @@ std::string number_to_string(Number x)
     if (m.empty()) {
         return "0";
     } else if (exp <= 0) {
-        return sign + "0." + std::string(-exp, '0') + m;
+        r = sign + "0." + std::string(-exp, '0') + m;
     } else if (static_cast<size_t>(exp) < m.length()) {
-        return sign + m.substr(0, exp) + "." + m.substr(exp);
+        r = sign + m.substr(0, exp) + "." + m.substr(exp);
     } else {
         return sign + m + std::string(exp - m.length(), '0');
     }
+    std::string::size_type i = r.length() - 1;
+    while (r[i] == '0') {
+        i--;
+    }
+    if (r[i] == '.') {
+        i--;
+    }
+    return r.substr(0, i+1);
 }
 
 uint8_t number_to_uint8(Number x)
 {
-    return 0;//bid128_to_uint8_int(x.x);
+    return mpfr_get_ui(x.x, MPFR_RNDN);
 }
 
 int8_t number_to_sint8(Number x)
 {
-    return 0;//bid128_to_int8_int(x.x);
+    return mpfr_get_si(x.x, MPFR_RNDN);
 }
 
 uint16_t number_to_uint16(Number x)
 {
-    return 0;//bid128_to_uint16_int(x.x);
+    return mpfr_get_ui(x.x, MPFR_RNDN);
 }
 
 int16_t number_to_sint16(Number x)
 {
-    return 0;//bid128_to_int16_int(x.x);
+    return mpfr_get_si(x.x, MPFR_RNDN);
 }
 
 uint32_t number_to_uint32(Number x)
 {
-    return 0;//bid128_to_uint32_int(x.x);
+    return mpfr_get_ui(x.x, MPFR_RNDN);
 }
 
 int32_t number_to_sint32(Number x)
 {
-    return 0;//bid128_to_int32_int(x.x);
+    return mpfr_get_si(x.x, MPFR_RNDN);
 }
 
 uint64_t number_to_uint64(Number x)
 {
-    return 0;//bid128_to_uint64_int(x.x);
+    return mpfr_get_uj(x.x, MPFR_RNDN);
 }
 
 int64_t number_to_sint64(Number x)
 {
-    return 0;//bid128_to_int64_int(x.x);
+    return mpfr_get_sj(x.x, MPFR_RNDN);
 }
 
 float number_to_float(Number x)
 {
-    return 0;//bid128_to_binary32(x.x);
+    return mpfr_get_flt(x.x, MPFR_RNDN);
 }
 
 double number_to_double(Number x)
 {
-    return 0;//bid128_to_binary64(x.x);
+    return mpfr_get_d(x.x, MPFR_RNDN);
 }
 
 Number number_from_string(const std::string &s)
@@ -361,50 +448,70 @@ Number number_from_string(const std::string &s)
 
 Number number_from_uint8(uint8_t x)
 {
-    return Number();//bid128_from_uint32(x);
+    Number r;
+    mpfr_set_ui(r.x, x, MPFR_RNDN);
+    return r;
 }
 
 Number number_from_sint8(int8_t x)
 {
-    return Number();//bid128_from_int32(x);
+    Number r;
+    mpfr_set_si(r.x, x, MPFR_RNDN);
+    return r;
 }
 
 Number number_from_uint16(uint16_t x)
 {
-    return Number();//bid128_from_uint32(x);
+    Number r;
+    mpfr_set_ui(r.x, x, MPFR_RNDN);
+    return r;
 }
 
 Number number_from_sint16(int16_t x)
 {
-    return Number();//bid128_from_int32(x);
+    Number r;
+    mpfr_set_si(r.x, x, MPFR_RNDN);
+    return r;
 }
 
 Number number_from_uint32(uint32_t x)
 {
-    return Number();//bid128_from_uint32(x);
+    Number r;
+    mpfr_set_ui(r.x, x, MPFR_RNDN);
+    return r;
 }
 
 Number number_from_sint32(int32_t x)
 {
-    return Number();//bid128_from_int32(x);
+    Number r;
+    mpfr_set_si(r.x, x, MPFR_RNDN);
+    return r;
 }
 
 Number number_from_uint64(uint64_t x)
 {
-    return Number();//bid128_from_uint64(x);
+    Number r;
+    mpfr_set_uj(r.x, x, MPFR_RNDN);
+    return r;
 }
 
 Number number_from_sint64(int64_t x)
 {
-    return Number();//bid128_from_int64(x);
+    Number r;
+    mpfr_set_sj(r.x, x, MPFR_RNDN);
+    return r;
 }
 
 Number number_from_float(float x)
 {
-    return Number();//binary32_to_bid128(x);
+    Number r;
+    mpfr_set_flt(r.x, x, MPFR_RNDN);
+    return r;
 }
 
 Number number_from_double(double x)
 {
-    return Number();//binary64_to_bid128(x);
+    Number r;
+    mpfr_set_d(r.x, x, MPFR_RNDN);
+    return r;
 }
