edb3bf0e89ada71d9f95ff8c2977bc8bc81bb551
Larry Frieson <larryf@mlinks.net>
2019-02-13 21:44:35 -0800

    Implement number to string functionality

--- exec/cnex/number.c
+++ exec/cnex/number.c
@@ -17,49 +17,125 @@
 char *number_to_string(Number x)
 {
     static char buf[50];
-    char val[50];
-    bid128_to_string(val, x);
-    sprintf(buf, "%g", atof(val));
+    number_toString(x, buf, sizeof(buf));
     return buf;
 }
 
-/* ToDo: Implement proper number to string formatter. */
 void number_toString(Number x, char *buf, size_t len)
 {
-    //const int PRECISION = 34; /* ToDo: Finish implementation */
-    int iLeadingZeros = 0;
+    memset(buf, 0x00, len);
+    const int PRECISION = 34;
 
     assert(len != 0);
 
-    char val[50];
-    char lead[50];
+    char val[50] = { 0 };
     bid128_to_string(val, x);
 
-    char *s = &val[0];
+    char *v, *s = v = &val[1];
 
-    /* Skip over the leading +, if there is one. */
-    while(*s != '\0' && (*s == '+')) s++;
-
-    char *v = s;
-
-    while (*s != '\0' && (*s != 'E')) s++;
-
-    char *E = s;
-
-    if (*s == 'E') {
-        /* Deal with the exponent. */
-        int i;
-        iLeadingZeros = abs(atoi(++s));
-        for (i = 0; i < iLeadingZeros; i++) {
-            lead[i] = '0';
-        }
-        lead[i++] = '.';
-        lead[i++] = '\0';
-        strcpy(buf, lead);
+    // Calculate the length of the string, locating the exponent value at the same time.
+    int slen = 0;
+    while (*s != '\0' && (*s != 'E')) {
+        s++;
+        slen++;
     }
 
+    // Store the position of the exponent marker.
+    char *E = s;
+
+    // If we didn't find an exponent marker, then just return the buffer that it is.
+    if (*s != 'E') {
+        buf = val;
+        return;
+    }
+
+    // Get the actual exponent, by walking past the eponent marker, then converting to decimal value.
+    int exponent = atoi(++s);
+
+    // Place a NULL char at the exponent marker location, effectively terminating the string there.
     *E = '\0';
-    strcat(buf, v);
+
+    // Now, we setup a (p)ointer to be one byte BEFORE the NULL we just added to the string, this
+    // effectively places us at the end of the numerical string.
+    char *p = E - 1;
+
+    // We're going to start walking from right to left, looking for the last significant digit; the last digit in the string that isn't zero.
+    while ((p >= v) && (*p == '0')) p--;
+
+    // Once found, we can note the position of it in the string.  Since we know that v is a pointer to the start of the string, and p is the last digit found.
+    const int last_significant_digit = (int)(p - v) + 1;
+
+    // Once we know the last siginificant digit, we can calculate how many trailing zero existed.
+    const int trailing_zeros = (int)slen - last_significant_digit;
+    exponent += trailing_zeros;
+    char mantissa[50] = { 0 };
+    char r[50] = { 0 };
+    // Copy the mantissa out to the last significant digit.
+    memcpy(mantissa, v, last_significant_digit);
+    // Guarantee the string is null terminated, despite the buffer starting off NULL.
+    mantissa[last_significant_digit] = '\0';
+
+    if (exponent != 0) {
+        if (exponent > 0 && last_significant_digit + exponent <= PRECISION) {
+            // We have an exponent value we need to deal with, but the length combined with the exponent
+            // is less than our desired max precision, so we move the mantissa value into (r)esult, the length
+            // of the significant mantissa, adding zeros as necessary.
+            memcpy(r, mantissa, last_significant_digit);
+            for (int i = last_significant_digit; i < (last_significant_digit + exponent); i++) {
+                r[i] = '0';
+            }
+        } else if (exponent < 0 && -exponent < slen) {
+            // Our exponent is negative, and is less than the length of the mantissa, so we're
+            // going to insert the decimal into slen+exponent location.
+            memcpy(r, mantissa, slen+exponent);
+            r[slen+exponent] = '.';
+            memcpy(&r[slen+exponent]+1, &mantissa[slen+exponent], slen - (slen+exponent));
+        } else if (exponent < 0 && -exponent == slen) {
+            // Our exponent is negative, and is the length of the mantissa, so we're going to start
+            // the number with "0.", and then add the rest of the mantissa on the right side of the decimal.
+            memcpy(r, "0.", 2);
+            memcpy(&r[2], mantissa, slen);
+        } else if (exponent < 0 && slen - exponent <= PRECISION) {
+            // Our exponent is negative, and the mantissa length, minus the exponent less or equal to our max PRECISION
+            // then we need to insert zero's to the right of the decimal.
+            int count = 2;
+            // Start out by setting [r]esult to "0."
+            memcpy(r, "0.", count);
+            // Now, we copy the necessary number of zeros after the decimal point...
+            for (int i = 0; i < -exponent-slen;i++, count++) {
+                r[count] = '0';
+            }
+            // Then copy the remainder of the mantissa to our [r]esult.
+            memcpy(&r[count], mantissa, slen);
+            // Ensure that [r]esult is null terminated.  I'm not 100% convinced this is necessary.
+            r[count+slen] = '\0';
+        } else {
+            // Our exponent is positive, therefore we need to add the decimal point on the right of the number.
+            exponent += last_significant_digit - 1;
+            if (last_significant_digit >= 3) {
+                // Our mantissa is at least 3 digits long, so copy the first digit to [r]esult
+                r[0] = mantissa[0];
+                // add the decimal point
+                r[1] = '.';
+                // copy the remainder of the mantissa to [r]esult
+                memcpy(&r[2], &mantissa[1], last_significant_digit-1);
+            }
+            // Now, create the actual exponent value.
+            char exp[36];
+            int explen = sprintf(exp, "e%d", exponent);
+            memcpy(&r[last_significant_digit+1], exp, explen+1); // Include the terminating NULL char given to us by sprintf()
+        }
+    } else {
+        // Exponent is zero, or just copy the mantissa, up to the last significant digit.
+        memcpy(r, mantissa, last_significant_digit+1); // Include terminating NULL char.
+    }
+    if (val[0] == '-') {
+        // The number is a negative number, so include the sign on the end of it.
+        memcpy(&buf[1], r, strlen(r)+1); // Be sure to include the terminating NULL char.
+        buf[0] = '-';
+        return;
+    }
+    memcpy(buf, r, strlen(r)+1); // Be sure to include the terminating NULL char.
 }
 
 Number number_from_string(char *s)
@@ -161,6 +237,11 @@ uint64_t number_to_uint64(Number x)
  * Number FROM functions
  */
 
+Number number_from_sint32(int32_t x)
+{
+    return bid128_from_int32(x);
+}
+
 Number number_from_uint32(uint32_t x)
 {
     return bid128_from_uint32(x);
@@ -195,6 +276,11 @@ BOOL number_is_odd(Number x)
 #ifdef __NUMBER_TESTS
 void main()
 {
+    Number x;
+    x = number_from_uint32(9);
+    assert(strcasecmp(number_to_string(x), "9") == 0);
+    x = number_from_uint32(20);
+    assert(strcasecmp(number_to_string(x), "20") == 0);
     assert(strcasecmp(number_to_string(number_from_string("1234.5678e5")), "123456780") == 0);
     assert(strcasecmp(number_to_string(number_from_string("1234.5678e4")), "12345678") == 0);
     assert(strcasecmp(number_to_string(number_from_string("1234.5678e3")), "1234567.8") == 0);
@@ -206,9 +292,25 @@ void main()
     assert(strcasecmp(number_to_string(number_from_string("1234.5678e-3")), "1.2345678") == 0);
     assert(strcasecmp(number_to_string(number_from_string("1234.5678e-4")), "0.12345678") == 0);
     assert(strcasecmp(number_to_string(number_from_string("1234.5678e-5")), "0.012345678") == 0);
+    assert(strcasecmp(number_to_string(number_from_string("-1234.5678e-2")), "-12.345678") == 0);
+    assert(strcasecmp(number_to_string(number_from_string("-12345678e5")), "-1234567800000") == 0);
+    assert(strcasecmp(number_to_string(number_from_string("-12345678e-10")), "-0.0012345678") == 0);
 
-    assert(strcasecmp(number_to_string(number_from_string("123.45")),      number_to_string(number_from_string("123.45"))) == 0);
-    assert(strcasecmp(number_to_string(number_from_string("12.345")),      number_to_string(number_from_string("123.45"))) != 0);
-    assert(strcasecmp(number_to_string(number_from_string("1234567890")),  number_to_string(number_from_string("1234567890"))) == 0);
+    assert(strcasecmp(number_to_string(number_from_string("-12345678e-36")), "-1.2345678e-29") == 0);
+    assert(strcasecmp(number_to_string(number_from_string("+12345678e-56")), "1.2345678e-49") == 0);
+    assert(strcasecmp(number_to_string(number_from_string("+12345678e56")), "1.2345678e63") == 0);
+    assert(strcasecmp(number_to_string(number_from_string("-12345678e56")), "-1.2345678e63") == 0);
+    assert(strcasecmp(number_to_string(number_from_string("-12345678e26")), "-1234567800000000000000000000000000") == 0);
+    assert(strcasecmp(number_to_string(number_from_string("-12345678e-26")), "-0.00000000000000000012345678") == 0);
+    assert(strcasecmp(number_to_string(number_from_string("+12345678e-26")), "0.00000000000000000012345678") == 0);
+    assert(strcasecmp(number_to_string(number_from_string("+12345678e26")), "1234567800000000000000000000000000") == 0);
+    assert(strcasecmp(number_to_string(number_from_string("+1234567800000000000000000000000001E-13")), "123456780000000000000.0000000000001") == 0);
+
+    assert(strcasecmp(number_to_string(number_from_string("-1234567800000000000000000000000001E-34")), "-0.1234567800000000000000000000000001") == 0);
+    assert(strcasecmp(number_to_string(number_from_string("-1234567800000000000000000000000001E-35")), "-1.234567800000000000000000000000001e-2") == 0);
+    assert(strcasecmp(number_to_string(number_from_string("-12345678000000000000000000000000E2")), "-1234567800000000000000000000000000") == 0);
+    assert(strcasecmp(number_to_string(number_from_string("-12345678000000000000000000000000E3")), "-1.2345678e34") == 0);
+    assert(strcasecmp(number_to_string(number_from_string("+12345678000000000000000000000000E2")), "1234567800000000000000000000000000") == 0);
+    assert(strcasecmp(number_to_string(number_from_string("+12345678000000000000000000000000E3")), "1.2345678e34") == 0);
 }
 #endif
