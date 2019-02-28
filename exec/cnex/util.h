58b154cf4aca7c4ad12f066496dade85fe58cbec
Greg Hewgill <greg@hewgill.com>
2019-03-01 11:12:16 +1300

    Add noreturn attribute to fatal_error

--- exec/cnex/util.h
+++ exec/cnex/util.h
@@ -5,7 +5,15 @@
 #define ENUM(x)     estr(x)
 
 void exec_error(const char *msg, ...);
-void fatal_error(const char *msg, ...);
+
+#ifdef _MSC_VER
+__declspec(noreturn)
+#endif
+void fatal_error(const char *msg, ...)
+#ifdef __GNUC__
+__attribute__((noreturn))
+#endif
+;
 
 typedef enum { FALSE, TRUE } BOOL;
 
