b6f11be0def39b6028b04f8b03a119c7f25423f6
Larry Frieson <larryf@mlinks.net>
2019-03-05 13:40:05 -0800

    Add io module to cnex

---
+++ exec/cnex/lib/io.h
@@ -0,0 +1,17 @@
+#ifndef IO_H
+#define IO_H
+
+struct tagTExecutor;
+
+void io_fprint(struct tagTExecutor *exec);
+void io_close(struct tagTExecutor *exec);
+void io_open(struct tagTExecutor *exec);
+void io_readBytes(struct tagTExecutor *exec);
+void io_readLine(struct tagTExecutor *exec);
+void io_seek(struct tagTExecutor *exec);
+void io_tell(struct tagTExecutor *exec);
+void io_truncate(struct tagTExecutor *exec);
+void io_write(struct tagTExecutor *exec);
+void io_writeBytes(struct tagTExecutor *exec);
+
+#endif
