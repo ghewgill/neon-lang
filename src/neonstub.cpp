dfb65690a0aa336dd4b6b92315f3c6c3fe7ffb9e
Greg Hewgill <greg@hewgill.com>
2020-01-24 12:23:50 +1300

    Suppress cppcheck warning about double assignment

--- src/neonstub.cpp
+++ src/neonstub.cpp
@@ -10,11 +10,13 @@ int main(int argc, char *argv[])
     bool enable_assert = true;
     unsigned short debug_port = 0;
 
-    const char *exe = argv[0];
+    const char *exe;
     #ifdef _WIN32
         char buf[MAX_PATH];
         GetModuleFileName(NULL, buf, sizeof(buf));
         exe = buf;
+    #else
+        exe = argv[0];
     #endif
     run_from_bundle(exe, enable_assert, debug_port, argc, argv);
 
