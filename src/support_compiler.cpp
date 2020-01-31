41786007347562b9b6f32fb7f133f20edfa9c72a
Greg Hewgill <greg@hewgill.com>
2020-01-31 13:52:35 +1300

    Refactor of loadBytecode to avoid recompile when not necessary

--- src/support_compiler.cpp
+++ src/support_compiler.cpp
@@ -32,42 +32,42 @@ bool CompilerSupport::loadBytecode(const std::string &name, Bytecode &object)
         return false;
     }
 
-    std::ifstream obj(names.second, std::ios::binary);
-    std::ifstream src(names.first);
+    std::ifstream obj_file(names.second, std::ios::binary);
+    std::ifstream src_file(names.first);
 
-    std::string source;
-    if (src.good()) {
+    std::string source_text;
+    if (src_file.good()) {
         std::stringstream buf;
-        buf << src.rdbuf();
-        source = buf.str();
+        buf << src_file.rdbuf();
+        source_text = buf.str();
     }
 
-    // TODO: skipping the following check causes modules to always be rebuilt
-    //if (obj.good() && not source.empty()) {
-    //    SHA256 sha256;
-    //    sha256(source);
-    //    unsigned char h[SHA256::HashBytes];
-    //    sha256.getHash(h);
-    //    std::string hash = std::string(h, h+sizeof(h));
-    //
-    //    std::stringstream buf;
-    //    buf << obj.rdbuf();
-    //    std::vector<unsigned char> bytecode;
-    //    std::string s = buf.str();
-    //    std::copy(s.begin(), s.end(), std::back_inserter(bytecode));
-    //    object = Bytecode(bytecode);
-    //
-    //    if (object.source_hash == hash) {
-    //        return true;
-    //    }
-    //}
+    if (not source_text.empty()) {
+        if (obj_file.good()) {
+            SHA256 sha256;
+            sha256(source_text);
+            unsigned char h[SHA256::HashBytes];
+            sha256.getHash(h);
+            std::string hash = std::string(h, h+sizeof(h));
 
-    if (not source.empty()) {
-        obj.close();
+            std::stringstream buf;
+            buf << obj_file.rdbuf();
+            std::vector<unsigned char> bytecode;
+            std::string s = buf.str();
+            std::copy(s.begin(), s.end(), std::back_inserter(bytecode));
+            object.load(name, bytecode);
+
+            if (object.source_hash == hash) {
+                return true;
+            }
+            object = Bytecode();
+        }
+
+        obj_file.close();
         const std::string objname = names.first + "x";
         remove(objname.c_str());
         {
-            auto tokens = tokenize(names.first, source);
+            auto tokens = tokenize(names.first, source_text);
             auto parsetree = parse(*tokens);
             auto ast = analyze(this, parsetree.get());
             auto bytecode = compile(ast, nullptr);
@@ -76,14 +76,14 @@ bool CompilerSupport::loadBytecode(const std::string &name, Bytecode &object)
                 cproc(this, ast);
             }
         }
-        obj.open(objname, std::ios::binary);
-        if (not obj.good()) {
+        obj_file.open(objname, std::ios::binary);
+        if (not obj_file.good()) {
             return false;
         }
     }
 
     std::stringstream buf;
-    buf << obj.rdbuf();
+    buf << obj_file.rdbuf();
     std::vector<unsigned char> bytecode;
     std::string s = buf.str();
     std::copy(s.begin(), s.end(), std::back_inserter(bytecode));
