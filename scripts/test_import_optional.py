0d8282f31b74b6546f07fa37e88b59ed12e945c8
Greg Hewgill <greg@hewgill.com>
2020-02-04 12:52:49 +1300

    Fix up import optional test to use temporary compiled file

--- scripts/test_import_optional.py
+++ scripts/test_import_optional.py
@@ -7,9 +7,9 @@ import sys
 neonc = sys.argv[1]
 executor = sys.argv[2:]
 
-out = subprocess.check_output([neonc, "t/import-optional-missing.neon"], env={"NEONPATH": "t/compile-time-only"}, stderr=subprocess.STDOUT, universal_newlines=True)
+out = subprocess.check_output([neonc, "-o", "tmp/import-optional-missing.neonx", "t/import-optional-missing.neon"], env={"NEONPATH": "t/compile-time-only"}, stderr=subprocess.STDOUT, universal_newlines=True)
 sys.stdout.write(out)
 if "not found" in out:
     print("{}: Failed: expected compile step to find module2.neon".format(sys.argv[0]), file=sys.stderr)
     sys.exit(1)
-subprocess.check_call(executor + ["t/import-optional-missing.neonx"])
+subprocess.check_call(executor + ["tmp/import-optional-missing.neonx"])
