222dfbb72136f93ddbea97f7b5260c2ac9bb5fe6
Greg Hewgill <greg@hewgill.com>
2020-07-10 10:35:02 +1200

    Update sqlite to 3.32.3

--- external/sqlite.cmake
+++ external/sqlite.cmake
@@ -1,6 +1,6 @@
-if (NOT EXISTS external/sqlite-amalgamation-3080803)
+if (NOT EXISTS external/sqlite-amalgamation-3320300)
     execute_process(
-        COMMAND python3 ../scripts/extract.py sqlite-amalgamation-3080803.zip .
+        COMMAND python3 ../scripts/extract.py sqlite-amalgamation-3320300.zip .
         WORKING_DIRECTORY ${CMAKE_CURRENT_SOURCE_DIR}
         RESULT_VARIABLE retcode
     )
@@ -9,11 +9,11 @@ if (NOT EXISTS external/sqlite-amalgamation-3080803)
     endif ()
 endif ()
 add_library(sqlite3
-    sqlite-amalgamation-3080803/sqlite3.c
+    sqlite-amalgamation-3320300/sqlite3.c
 )
 if (${CMAKE_SYSTEM_NAME} STREQUAL "Linux" OR ${CMAKE_SYSTEM_NAME} STREQUAL "OpenBSD" OR ${CMAKE_SYSTEM_NAME} STREQUAL "FreeBSD")
     target_link_libraries(sqlite3 pthread)
 endif ()
 target_include_directories(sqlite3
-    INTERFACE sqlite-amalgamation-3080803
+    INTERFACE sqlite-amalgamation-3320300
 )
