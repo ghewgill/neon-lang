c6dbe99dee6edf521246dadcfe234c7a6126a39d
Greg Hewgill <greg@hewgill.com>
2020-08-18 10:38:30 +0000

    Add some jvm math functions

--- rtl/jvm/neon/Math.java
+++ rtl/jvm/neon/Math.java
@@ -8,12 +8,36 @@ public class Math {
         return new neon.type.Number(x.repr.abs());
     }
 
+    public static neon.type.Number acos(neon.type.Number x) {
+        return new neon.type.Number(java.lang.Math.acos(x.repr.doubleValue()));
+    }
+
+    public static neon.type.Number asin(neon.type.Number x) {
+        return new neon.type.Number(java.lang.Math.asin(x.repr.doubleValue()));
+    }
+
+    public static neon.type.Number atan(neon.type.Number x) {
+        return new neon.type.Number(java.lang.Math.atan(x.repr.doubleValue()));
+    }
+
     public static neon.type.Number ceil(neon.type.Number x) {
-        return new neon.type.Number(x.repr.divide(BigDecimal.ONE, BigDecimal.ROUND_CEILING));
+        return new neon.type.Number(x.repr.setScale(0, BigDecimal.ROUND_CEILING));
+    }
+
+    public static neon.type.Number cos(neon.type.Number x) {
+        return new neon.type.Number(java.lang.Math.cos(x.repr.doubleValue()));
+    }
+
+    public static neon.type.Number exp(neon.type.Number x) {
+        return new neon.type.Number(java.lang.Math.exp(x.repr.doubleValue()));
     }
 
     public static neon.type.Number floor(neon.type.Number x) {
-        return new neon.type.Number(x.repr.divide(BigDecimal.ONE, BigDecimal.ROUND_FLOOR));
+        return new neon.type.Number(x.repr.setScale(0, BigDecimal.ROUND_FLOOR));
+    }
+
+    public static neon.type.Number log(neon.type.Number x) {
+        return new neon.type.Number(java.lang.Math.log(x.repr.doubleValue()));
     }
 
     public static neon.type.Number intdiv(neon.type.Number x, neon.type.Number y) {
@@ -45,6 +69,18 @@ public class Math {
 
     public static neon.type.Number sign(neon.type.Number x) {
         return new neon.type.Number(x.repr.signum());
+    }
+
+    public static neon.type.Number sin(neon.type.Number x) {
+        return new neon.type.Number(java.lang.Math.sin(x.repr.doubleValue()));
+    }
+
+    public static neon.type.Number sqrt(neon.type.Number x) {
+        return new neon.type.Number(java.lang.Math.sqrt(x.repr.doubleValue()));
+    }
+
+    public static neon.type.Number tan(neon.type.Number x) {
+        return new neon.type.Number(java.lang.Math.tan(x.repr.doubleValue()));
     }
 
     public static neon.type.Number trunc(neon.type.Number x) {
