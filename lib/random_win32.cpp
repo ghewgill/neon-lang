0cfe484b3a2cc243c9f5d46ea59d856c76241ff5
Larry Frieson <larryf@mlinks.net>
2020-09-16 19:43:54 -0700

    Add random.bytes() win32 implementation

--- lib/random_win32.cpp
+++ lib/random_win32.cpp
@@ -1,6 +1,9 @@
 #include <vector>
+#include <windows.h>
+#include <wincrypt.h>
 
 #include "number.h"
+#include "rtl_exec.h"
 
 namespace rtl {
 
@@ -10,7 +13,20 @@ std::vector<unsigned char> bytes(Number count)
 {
     size_t icount = number_to_uint32(count);
     std::vector<unsigned char> r(icount);
-    // Get actual random bytes here.
+
+    HCRYPTPROV hCryptProv;
+    if (!CryptAcquireContext(&hCryptProv, NULL, NULL, PROV_RSA_FULL, 0)) {
+        // Should we return GetLastError() in the exception too?
+        throw RtlException(Exception_RandomException, utf8string("could not acquire default crypto context"));
+    }
+    unsigned char *p = r.data();
+    if (!CryptGenRandom(hCryptProv, static_cast<DWORD>(icount), p)) {
+        CryptReleaseContext(hCryptProv, 0);
+        // Should we return GetLastError() in the exception too?
+        throw RtlException(Exception_RandomException, utf8string("failed to get random data"));
+    }
+    CryptReleaseContext(hCryptProv, 0);
+
     return r;
 }
 
