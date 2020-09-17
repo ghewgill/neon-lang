9b2c6254c7dcd9c780ec5e85e174ced1368214e6
Greg Hewgill <greg@hewgill.com>
2020-09-17 14:09:07 +1200

    - add RandomException

--- lib/random_posix.cpp
+++ lib/random_posix.cpp
@@ -3,6 +3,7 @@
 #include <vector>
 
 #include "number.h"
+#include "rtl_exec.h"
 
 namespace rtl {
 
@@ -13,17 +14,21 @@ std::vector<unsigned char> bytes(Number count)
     size_t icount = number_to_uint32(count);
     std::vector<unsigned char> r(icount);
     int fd = open("/dev/urandom", O_RDONLY);
-    if (fd >= 0) {
-        unsigned char *p = r.data();
-        while (icount > 0) {
-            ssize_t n = read(fd, p, icount);
-            icount -= n;
-            p += n;
-        }
-        close(fd);
-        return r;
+    if (fd < 0) {
+        throw RtlException(Exception_RandomException, utf8string("could not open /dev/urandom"));
     }
-    return {};
+    unsigned char *p = r.data();
+    while (icount > 0) {
+        ssize_t n = read(fd, p, icount);
+        if (n < 1) {
+            close(fd);
+            throw RtlException(Exception_RandomException, utf8string("error readingn from /dev/urandom"));
+        }
+        icount -= n;
+        p += n;
+    }
+    close(fd);
+    return r;
 }
 
 } // namespace ne_random
