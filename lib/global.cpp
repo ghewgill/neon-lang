165e1ce32527019299bd0e93a4818e594a03920f
Greg Hewgill <greg@hewgill.com>
2020-11-20 22:14:54 +1300

    Move Object related classes to own module

--- lib/global.cpp
+++ lib/global.cpp
@@ -478,172 +478,30 @@ std::shared_ptr<Object> object__makeNull()
     return std::shared_ptr<Object>(nullptr);
 }
 
-class ObjectBoolean: public Object {
-public:
-    explicit ObjectBoolean(bool b): b(b) {}
-    virtual bool getBoolean(bool &r) const override { r = b; return true; }
-    virtual utf8string toString() const override { return utf8string(b ? "TRUE" : "FALSE"); }
-private:
-    const bool b;
-private:
-    ObjectBoolean(const ObjectBoolean &);
-    ObjectBoolean &operator=(const ObjectBoolean &);
-};
-
 std::shared_ptr<Object> object__makeBoolean(bool b)
 {
     return std::shared_ptr<Object>(new ObjectBoolean(b));
 }
-
-class ObjectNumber: public Object {
-public:
-    explicit ObjectNumber(Number n): n(n) {}
-    virtual bool getNumber(Number &r) const override { r = n; return true; }
-    virtual utf8string toString() const override { return utf8string(number_to_string(n)); }
-private:
-    const Number n;
-private:
-    ObjectNumber(const ObjectNumber &);
-    ObjectNumber &operator=(const ObjectNumber &);
-};
 
 std::shared_ptr<Object> object__makeNumber(Number n)
 {
     return std::shared_ptr<Object>(new ObjectNumber(n));
 }
 
-class ObjectString: public Object {
-public:
-    explicit ObjectString(const utf8string &s): s(s) {}
-    virtual bool getString(utf8string &r) const override { r = s; return true; }
-    // TODO: Use quoting function to quote the value properly.
-    virtual utf8string toLiteralString() const override { return "\"" + s + "\""; }
-    virtual utf8string toString() const override { return s; }
-private:
-    const utf8string s;
-private:
-    ObjectString(const ObjectString &);
-    ObjectString &operator=(const ObjectString &);
-};
-
 std::shared_ptr<Object> object__makeString(const utf8string &s)
 {
     return std::shared_ptr<Object>(new ObjectString(s));
 }
-
-class ObjectBytes: public Object {
-public:
-    explicit ObjectBytes(const std::vector<unsigned char> &b): b(b) {}
-    virtual bool getBytes(std::vector<unsigned char> &r) const override { r = b; return true; }
-    virtual utf8string toString() const override {
-        utf8string r {"HEXBYTES \""};
-        bool first = true;
-        for (auto x: b) {
-            if (first) {
-                first = false;
-            } else {
-                r += ' ';
-            }
-            char buf[3];
-            snprintf(buf, sizeof(buf), "%02x", x);
-            r += buf;
-        }
-        r += "\"";
-        return r;
-    }
-private:
-    const std::vector<unsigned char> b;
-private:
-    ObjectBytes(const ObjectBytes &);
-    ObjectBytes &operator=(const ObjectBytes &);
-};
 
 std::shared_ptr<Object> object__makeBytes(const std::vector<unsigned char> &b)
 {
     return std::shared_ptr<Object>(new ObjectBytes(b));
 }
 
-class ObjectArray: public Object {
-public:
-    explicit ObjectArray(const std::vector<std::shared_ptr<Object>> &a): a(a) {}
-    virtual bool getArray(std::vector<std::shared_ptr<Object>> &r) const override { r = a; return true; }
-    virtual bool subscript(std::shared_ptr<Object> index, std::shared_ptr<Object> &r) const override {
-        Number i;
-        if (not index->getNumber(i)) {
-            throw RtlException(Exception_DynamicConversionException, utf8string("to Number"));
-        }
-        uint64_t ii = number_to_uint64(i);
-        if (ii >= a.size()) {
-            throw RtlException(Exception_ArrayIndexException, utf8string(number_to_string(i)));
-        }
-        r = a.at(ii);
-        return true;
-    }
-    virtual utf8string toString() const override {
-        utf8string r {"["};
-        bool first = true;
-        for (auto x: a) {
-            if (not first) {
-                r.append(", ");
-            } else {
-                first = false;
-            }
-            r.append(x != nullptr ? x->toLiteralString() : utf8string("null"));
-        }
-        r.append("]");
-        return r;
-    }
-private:
-    std::vector<std::shared_ptr<Object>> a;
-private:
-    ObjectArray(const ObjectArray &);
-    ObjectArray &operator=(const ObjectArray &);
-};
-
 std::shared_ptr<Object> object__makeArray(std::vector<std::shared_ptr<Object>> a)
 {
     return std::shared_ptr<Object>(new ObjectArray(a));
 }
-
-class ObjectDictionary: public Object {
-public:
-    explicit ObjectDictionary(const std::map<utf8string, std::shared_ptr<Object>> &d): d(d) {}
-    virtual bool getDictionary(std::map<utf8string, std::shared_ptr<Object>> &r) const override { r = d; return true; }
-    virtual bool subscript(std::shared_ptr<Object> index, std::shared_ptr<Object> &r) const override {
-        utf8string i;
-        if (not index->getString(i)) {
-            throw RtlException(Exception_DynamicConversionException, utf8string("to String"));
-        }
-        auto e = d.find(i);
-        if (e == d.end()) {
-            return false;
-        }
-        r = e->second;
-        return true;
-    }
-    virtual utf8string toString() const override {
-        utf8string r {"{"};
-        bool first = true;
-        for (auto x: d) {
-            if (not first) {
-                r.append(", ");
-            } else {
-                first = false;
-            }
-            r.append("\"");
-            r.append(x.first.c_str());
-            r.append("\": ");
-            r.append(x.second != nullptr ? x.second->toLiteralString() : utf8string("null"));
-        }
-        r.append("}");
-        return r;
-    }
-private:
-    std::map<utf8string, std::shared_ptr<Object>> d;
-private:
-    ObjectDictionary(const ObjectDictionary &);
-    ObjectDictionary &operator=(const ObjectDictionary &);
-};
 
 std::shared_ptr<Object> object__makeDictionary(std::map<utf8string, std::shared_ptr<Object>> d)
 {
