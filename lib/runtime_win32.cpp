a46c9dd4f2d0d6108f852f3912a6bd923dcb7afb
Greg Hewgill <greg@hewgill.com>
2020-11-20 22:15:15 +1300

    wip

--- lib/runtime_win32.cpp
+++ lib/runtime_win32.cpp
@@ -2,6 +2,7 @@
 
 #include "cell.h"
 #include "exec.h"
+#include "object.h"
 
 class ComObject: public Object {
 public:
@@ -14,11 +15,12 @@ public:
     ~ComObject() {
         obj->Release();
     }
-    virtual bool invokeMethod(const utf8string &name, const std::vector<std::shared_ptr<Object>> &args, std::shared_ptr<Object> &result) override;
+    virtual bool invokeMethod(const utf8string &name, const std::vector<std::shared_ptr<Object>> &args, std::shared_ptr<Object> &result) const override;
+    virtual bool subscript(std::shared_ptr<Object>, std::shared_ptr<Object> &) const override;
     virtual utf8string toString() const { return utf8string("<ComObject:" + name + ">"); }
 };
 
-bool ComObject::invokeMethod(const utf8string &name, const std::vector<std::shared_ptr<Object>> &args, std::shared_ptr<Object> &result)
+bool ComObject::invokeMethod(const utf8string &name, const std::vector<std::shared_ptr<Object>> &args, std::shared_ptr<Object> &result) const
 {
     if (obj == NULL) {
         return false; // TODO: Exception
@@ -38,6 +40,40 @@ bool ComObject::invokeMethod(const utf8string &name, const std::vector<std::shar
     r = obj->Invoke(dispid, IID_NULL, GetUserDefaultLCID(), DISPATCH_METHOD, &params, &rv, NULL, NULL);
     if (r != S_OK) {
         return false; // TODO: Exception
+    }
+    return true;
+}
+
+bool ComObject::subscript(std::shared_ptr<Object> index, std::shared_ptr<Object> &result) const
+{
+    if (obj == NULL) {
+        return false; // TODO: Exception
+    }
+    OLECHAR wname[200];
+    MultiByteToWideChar(CP_UTF8, 0, name.c_str(), -1, wname, sizeof(wname)/sizeof(wname[0]));
+    OLECHAR *a = wname;
+    DISPID dispid;
+    HRESULT r = obj->GetIDsOfNames(IID_NULL, &a, 1, GetUserDefaultLCID(), &dispid);
+    if (r != S_OK) {
+        return false; // TODO: Exception
+    }
+    DISPPARAMS params = {NULL, NULL, 0, 0};
+    VARIANT rv;
+    VariantInit(&rv);
+    // TODO: exceptions, argument errors
+    r = obj->Invoke(dispid, IID_NULL, GetUserDefaultLCID(), DISPATCH_PROPERTYGET, &params, &rv, NULL, NULL);
+    if (r != S_OK) {
+        return false; // TODO: Exception
+    }
+    switch (rv.vt) {
+        case VT_BSTR: {
+            char buf[200];
+            WideCharToMultiByte(CP_UTF8, 0, rv.bstrVal, -1, buf, sizeof(buf), NULL, NULL);
+            result = std::make_shared<ObjectString>(utf8string(buf));
+            break;
+        }
+        default:
+            return false; // TODO: Exception
     }
     return true;
 }
