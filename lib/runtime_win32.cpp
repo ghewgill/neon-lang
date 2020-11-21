7fc6c4cb21a1c84011a02b482e643985cfa69067
Greg Hewgill <greg@hewgill.com>
2020-11-21 22:40:42 +1300

    wip

--- lib/runtime_win32.cpp
+++ lib/runtime_win32.cpp
@@ -21,6 +21,69 @@ public:
     virtual utf8string toString() const { return utf8string("<ComObject:" + name + ">"); }
 };
 
+void VariantFromObject(VARIANT *v, std::shared_ptr<Object> obj)
+{
+    if (obj == nullptr) {
+        v->vt = VT_NULL;
+        return;
+    }
+    bool b;
+    if (obj->getBoolean(b)) {
+        v->vt = VT_BOOL;
+        v->boolVal = b ? -1 : 0;
+        return;
+    }
+    Number n;
+    if (obj->getNumber(n)) {
+        if (number_is_integer(n)) {
+            v->vt = VT_I8;
+            v->llVal = number_to_sint64(n);
+        } else {
+            v->vt = VT_R8;
+            v->dblVal = number_to_double(n);
+        }
+        return;
+    }
+    utf8string s;
+    if (obj->getString(s)) {
+        v->vt = VT_BSTR;
+        OLECHAR wstr[200];
+        MultiByteToWideChar(CP_UTF8, 0, s.c_str(), -1, wstr, sizeof(wstr)/sizeof(wstr[0]));
+        v->bstrVal = SysAllocString(wstr);
+        return;
+    }
+    ComObject *co = dynamic_cast<ComObject *>(obj.get());
+    if (co != nullptr) {
+        v->vt = VT_DISPATCH;
+        v->pdispVal = co->obj;
+        co->obj->AddRef();
+        return;
+    }
+}
+
+std::shared_ptr<Object> ObjectFromVariant(VARIANT &v)
+{
+    switch (v.vt) {
+        case VT_NULL:
+            return nullptr;
+        case VT_BOOL:
+            return std::make_shared<ObjectBoolean>(v.boolVal != 0);
+        case VT_I1:
+            return std::make_shared<ObjectNumber>(number_from_sint8(v.bVal));
+        case VT_I4:
+            return std::make_shared<ObjectNumber>(number_from_sint32(v.lVal));
+        case VT_BSTR: {
+            char buf[200];
+            WideCharToMultiByte(CP_UTF8, 0, v.bstrVal, -1, buf, sizeof(buf), NULL, NULL);
+            return std::make_shared<ObjectString>(utf8string(buf));
+        }
+        case VT_DISPATCH:
+            v.pdispVal->AddRef();
+            return std::make_shared<ComObject>("IDispatch", v.pdispVal);
+    }
+    return nullptr;
+}
+
 bool ComObject::invokeMethod(const utf8string &methodname, const std::vector<std::shared_ptr<Object>> &args, std::shared_ptr<Object> &result) const
 {
     if (obj == NULL) {
@@ -35,13 +98,26 @@ bool ComObject::invokeMethod(const utf8string &methodname, const std::vector<std
         return false; // TODO: Exception
     }
     DISPPARAMS params = {NULL, NULL, 0, 0};
+    params.cArgs = args.size();
+    params.rgvarg = new VARIANTARG[params.cArgs];
+    for (size_t i = 0; i < args.size(); i++) {
+        VARIANT *v = &params.rgvarg[params.cArgs - 1 - i];
+        VariantInit(v);
+        VariantFromObject(v, args[i]);
+    }
     VARIANT rv;
     VariantInit(&rv);
     // TODO: exceptions, argument errors
     r = obj->Invoke(dispid, IID_NULL, GetUserDefaultLCID(), DISPATCH_METHOD, &params, &rv, NULL, NULL);
+    for (size_t i = 0; i < args.size(); i++) {
+        VariantClear(&params.rgvarg[i]);
+    }
+    delete[] params.rgvarg;
     if (r != S_OK) {
         return false; // TODO: Exception
     }
+    result = ObjectFromVariant(rv);
+    VariantClear(&rv);
     return true;
 }
 
@@ -70,16 +146,8 @@ bool ComObject::subscript(std::shared_ptr<Object> index, std::shared_ptr<Object>
     if (r != S_OK) {
         return false; // TODO: Exception
     }
-    switch (rv.vt) {
-        case VT_BSTR: {
-            char buf[200];
-            WideCharToMultiByte(CP_UTF8, 0, rv.bstrVal, -1, buf, sizeof(buf), NULL, NULL);
-            result = std::make_shared<ObjectString>(utf8string(buf));
-            break;
-        }
-        default:
-            return false; // TODO: Exception
-    }
+    result = ObjectFromVariant(rv);
+    VariantClear(&rv);
     return true;
 }
 
