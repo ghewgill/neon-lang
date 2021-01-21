7592e335b649af66c0984400bbd4b03c7b166ca3
Larry Frieson <larryf@mlinks.net>
2021-01-20 18:24:44 -0800

    Add better information to exceptions raised during runtime

--- exec/csnex/csnex.cs
+++ exec/csnex/csnex.cs
@@ -76,12 +76,16 @@ namespace csnex
             fs.Read(code, 0, (int)nSize);
             fs.Close();
 
-            Bytecode bc = new Bytecode();
-            bc.LoadBytecode(gOptions.Filename, code);
+            try {
+                Bytecode bc = new Bytecode();
+                bc.LoadBytecode(gOptions.Filename, code);
 
-            Executor exec = new Executor(bc);
-            retval = exec.Run(gOptions.EnableAssertions);
-
+                Executor exec = new Executor(bc);
+                retval = exec.Run(gOptions.EnableAssertions);
+            } catch (Exception ex) {
+                Console.Error.WriteLine(ex.Message);
+                return 1;
+            }
             return retval;
         }
     }
