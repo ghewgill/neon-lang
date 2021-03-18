4564967108790490ab2179daf88838431bbf7f77
Larry Frieson <larryf@mlinks.net>
2021-03-17 20:40:22 -0700

    Add Neon runtime information to NeonException

--- exec/csnex/Exceptions.cs
+++ exec/csnex/Exceptions.cs
@@ -1,4 +1,6 @@
 ﻿using System;
+using System.Runtime.Serialization;
+using System.Security.Permissions;
 
 namespace csnex
 {
@@ -6,6 +8,11 @@ namespace csnex
     public class NeonException: ApplicationException
     {
         public NeonException() {
+        }
+
+        public NeonException(string name, string info) : base(name) {
+            Name = name;
+            Info = info;
         }
 
         public NeonException(string message) : base(message) {
@@ -16,6 +23,21 @@ namespace csnex
 
         public NeonException(string message, System.Exception innerException) : base(message, innerException) {
         }
+
+        // Satisfy Warning CA2240 to implement a GetObjectData() to our custom exception type.
+        [SecurityPermission(SecurityAction.LinkDemand, Flags = SecurityPermissionFlag.SerializationFormatter)]
+        public override void GetObjectData(SerializationInfo info, StreamingContext context)
+        {
+            if (info == null) {
+                throw new ArgumentNullException("info");
+            }
+            info.AddValue("NeonException", Name);
+            info.AddValue("NeonInfo", Info);
+            base.GetObjectData(info, context);
+        }
+
+        public string Name;
+        public string Info;
     }
 
     [Serializable]
@@ -45,6 +67,18 @@ namespace csnex
         }
 
         public NotImplementedException(string message) : base(message) {
+        }
+    }
+
+    [Serializable]
+    public class NeonRuntimeException: NeonException
+    {
+        public NeonRuntimeException()
+        {
+        }
+
+        public NeonRuntimeException(string name, string info) : base(name, info)
+        {
         }
     }
 }
