2b7551c1c92049f26abbe8d1283bd864535e86f3
Greg Hewgill <greg@hewgill.com>
2021-06-04 11:53:13 +1200

    wip neonbuild

--- scripts/run_c.py
+++ scripts/run_c.py
@@ -4,22 +4,18 @@ import os
 import subprocess
 import sys
 
-neonc = os.path.join("bin", "neonc")
+neon = os.path.join("bin", "neon")
 
 i = 1
 while i < len(sys.argv):
-    if sys.argv[i] == "--neonc":
+    if sys.argv[i] == "--neon":
         i += 1
-        neonc = sys.argv[i]
+        neon = sys.argv[i]
     else:
         break
     i += 1
 
 fullname = sys.argv[i]
 
-subprocess.check_call([neonc, "-q", "-t", "c", fullname])
-if sys.platform == "win32":
-    subprocess.check_call(["cl", "/Fe{}".format(fullname.replace(".neon", "")), "/EHsc", "/Irtl/c", fullname.replace(".neon", ".c"), "rtl/c/neon.c"], stdout=subprocess.PIPE, stderr=subprocess.PIPE)
-else:
-    subprocess.check_call(["cc", "-std=c99", "-pedantic", "-Wall", "-Werror", "-o", fullname.replace(".neon", ""), "-Irtl/c", fullname.replace(".neon", ".c"), "rtl/c/neon.c", "-lm"])
+subprocess.check_call([neon, "tools/neonbuild.neon", fullname])
 subprocess.check_call([fullname.replace(".neon", "")] + sys.argv[i+1:])
