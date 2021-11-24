39ea2277a1fcf5b3ad179a9eda09b077f84ee267
Greg Hewgill <greg@hewgill.com>
2021-11-25 09:12:49 +1300

    wip ErrorInfo

--- rtl/jvm/neon/File.java
+++ rtl/jvm/neon/File.java
@@ -2,17 +2,49 @@ package neon;
 
 public class File {
 
+    static neon.lib.file$ErrorInfo make_error_info(Exception x, java.lang.String path) {
+        neon.lib.file$Error type = neon.lib.file$Error.other;
+        if (x instanceof java.nio.file.FileAlreadyExistsException) {
+            type = neon.lib.file$Error.alreadyExists;
+        } else if (x instanceof java.io.FileNotFoundException) {
+            type = neon.lib.file$Error.notFound;
+        } else if (x instanceof java.nio.file.AccessDeniedException) {
+            type = neon.lib.file$Error.permissionDenied;
+        }
+        return new neon.lib.file$ErrorInfo(type, neon.type.Number.ZERO, x.toString(), path);
+    }
+
+    static neon.lib.file$FileResult error_FileResult(Exception x, java.lang.String path) {
+        neon.lib.file$FileResult r = new neon.lib.file$FileResult();
+        r._choice = 1; // error
+        r.error = make_error_info(x, path);
+        return r;
+    }
+
+    static neon.lib.file$BytesResult error_BytesResult(Exception x, java.lang.String path) {
+        neon.lib.file$BytesResult r = new neon.lib.file$BytesResult();
+        r._choice = 1; // error
+        r.error = make_error_info(x, path);
+        return r;
+    }
+
+    static neon.lib.file$LinesResult error_LinesResult(Exception x, java.lang.String path) {
+        neon.lib.file$LinesResult r = new neon.lib.file$LinesResult();
+        r._choice = 1; // error
+        r.error = make_error_info(x, path);
+        return r;
+    }
+
     public static neon.lib.file$FileResult copy(java.lang.String filename, java.lang.String destination) {
         try {
             java.nio.file.Files.copy(
                 java.nio.file.FileSystems.getDefault().getPath(filename),
                 java.nio.file.FileSystems.getDefault().getPath(destination)
             );
+        } catch (java.nio.file.FileAlreadyExistsException x) {
+            return error_FileResult(x, destination);
         } catch (java.io.IOException x) {
-            neon.lib.file$FileResult r = new neon.lib.file$FileResult();
-            r._choice = 1; // error
-            r.error = "FileException: " + x;
-            return r;
+            return error_FileResult(x, filename);
         }
         neon.lib.file$FileResult r = new neon.lib.file$FileResult();
         r._choice = 0; // ok
@@ -27,10 +59,7 @@ public class File {
                 java.nio.file.StandardCopyOption.REPLACE_EXISTING
             );
         } catch (java.io.IOException x) {
-            neon.lib.file$FileResult r = new neon.lib.file$FileResult();
-            r._choice = 1; // error
-            r.error = "FileException: " + x;
-            return r;
+            return error_FileResult(x, filename);
         }
         neon.lib.file$FileResult r = new neon.lib.file$FileResult();
         r._choice = 0; // ok
@@ -57,10 +86,7 @@ public class File {
         try {
             in = new java.io.FileInputStream(filename);
         } catch (java.io.FileNotFoundException x) {
-            neon.lib.file$BytesResult r = new neon.lib.file$BytesResult();
-            r._choice = 1; // error
-            r.error = x.toString();
-            return r;
+            return error_BytesResult(x, filename);
         }
         byte[] buf = new byte[4096];
         int i = 0;
@@ -79,10 +105,7 @@ public class File {
             }
             in.close();
         } catch (java.io.IOException x) {
-            neon.lib.file$BytesResult r = new neon.lib.file$BytesResult();
-            r._choice = 1; // error
-            r.error = x.toString();
-            return r;
+            return error_BytesResult(x, filename);
         }
         byte[] data = new byte[i];
         System.arraycopy(buf, 0, data, 0, i);
@@ -97,10 +120,7 @@ public class File {
         try {
             br = new java.io.BufferedReader(new java.io.InputStreamReader(new java.io.FileInputStream(filename)));
         } catch (java.io.FileNotFoundException x) {
-            neon.lib.file$LinesResult r = new neon.lib.file$LinesResult();
-            r._choice = 1; // error
-            r.error = x.toString();
-            return r;
+            return error_LinesResult(x, filename);
         }
         neon.type.Array a = new neon.type.Array();
         try {
@@ -113,10 +133,7 @@ public class File {
             }
             br.close();
         } catch (java.io.IOException x) {
-            neon.lib.file$LinesResult r = new neon.lib.file$LinesResult();
-            r._choice = 1; // error
-            r.error = x.toString();
-            return r;
+            return error_LinesResult(x, filename);
         }
         neon.lib.file$LinesResult r = new neon.lib.file$LinesResult();
         r._choice = 0; // lines
@@ -129,19 +146,13 @@ public class File {
         try {
             out = new java.io.FileOutputStream(filename);
         } catch (java.io.FileNotFoundException x) {
-            neon.lib.file$FileResult r = new neon.lib.file$FileResult();
-            r._choice = 1; // error
-            r.error = x.toString();
-            return r;
+            return error_FileResult(x, filename);
         }
         try {
             out.write(data);
             out.close();
         } catch (java.io.IOException x) {
-            neon.lib.file$FileResult r = new neon.lib.file$FileResult();
-            r._choice = 1; // error
-            r.error = x.toString();
-            return r;
+            return error_FileResult(x, filename);
         }
         neon.lib.file$FileResult r = new neon.lib.file$FileResult();
         r._choice = 0; // ok
@@ -153,10 +164,7 @@ public class File {
         try {
             pw = new java.io.PrintWriter(new java.io.FileOutputStream(filename));
         } catch (java.io.FileNotFoundException x) {
-            neon.lib.file$FileResult r = new neon.lib.file$FileResult();
-            r._choice = 1; // error
-            r.error = x.toString();
-            return r;
+            return error_FileResult(x, filename);
         }
         for (Object s: data) {
             pw.println(s.toString());
