c30d969d71548db7757eb73a92a4b62f550af778
Greg Hewgill <greg@hewgill.com>
2022-03-22 15:36:59 +1300

    Fix JVM ExceptionInfo declaration

--- rtl/jvm/neon/type/NeonException.java
+++ rtl/jvm/neon/type/NeonException.java
@@ -4,23 +4,15 @@ public class NeonException extends RuntimeException {
     public NeonException(String name) {
         this.name = name;
         this.info = "";
-        this.code = 0;
     }
     public NeonException(String name, String info) {
         this.name = name;
         this.info = info;
-        this.code = 0;
-    }
-    public NeonException(String name, String info, int code) {
-        this.name = name;
-        this.info = info;
-        this.code = code;
     }
     public String toString() {
-        return name + " (" + info + ") (code " + code + ")";
+        return name + " (" + info + ")";
     }
 
     public final String name;
-    public final String info;
-    public final int code;
+    public final Object info;
 }
