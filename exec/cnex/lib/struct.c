bf5bb08d2cef40142236dec2835404816f57f301
Larry Frieson <larryf@mlinks.net>
2024-02-23 09:15:02 -0800

    Add endian-based float packing to cnex

--- exec/cnex/lib/struct.c
+++ exec/cnex/lib/struct.c
@@ -11,47 +11,139 @@
 
 
 
-void struct_packIEEE32(struct tagTExecutor *exec)
+void struct_packIEEE32BE(struct tagTExecutor *exec)
 {
     Number n = top(exec->stack)->number; pop(exec->stack);
 
     float x = number_to_float(n);
     Cell *r = cell_createStringCell(sizeof(x));
-    memcpy(r->string->data, &x, sizeof(x));
+    uint32_t i = *(uint32_t *)&x;
+    r->string->data[0] = (i >> 24) & 0xff;
+    r->string->data[1] = (i >> 16) & 0xff;
+    r->string->data[2] = (i >>  8) & 0xff;
+    r->string->data[3] = (i      ) & 0xff;
     r->type = cBytes;
 
     push(exec->stack, r);
 }
 
-void struct_packIEEE64(struct tagTExecutor *exec)
+void struct_packIEEE32LE(struct tagTExecutor *exec)
+{
+    Number n = top(exec->stack)->number; pop(exec->stack);
+
+    float x = number_to_float(n);
+    Cell *r = cell_createStringCell(sizeof(x));
+    uint32_t i = *(uint32_t *)&x;
+    r->string->data[3] = (i >> 24) & 0xff;
+    r->string->data[2] = (i >> 16) & 0xff;
+    r->string->data[1] = (i >>  8) & 0xff;
+    r->string->data[0] = (i      ) & 0xff;
+    r->type = cBytes;
+
+    push(exec->stack, r);
+}
+
+void struct_packIEEE64BE(struct tagTExecutor *exec)
 {
     Number n = top(exec->stack)->number; pop(exec->stack);
 
     double x = number_to_double(n);
     Cell *r = cell_createStringCell(sizeof(x));
-    memcpy(r->string->data, &x, sizeof(x));
+    uint64_t i = *(uint64_t *)&x;
+    r->string->data[0] = (i >> 56) & 0xff;
+    r->string->data[1] = (i >> 48) & 0xff;
+    r->string->data[2] = (i >> 40) & 0xff;
+    r->string->data[3] = (i >> 32) & 0xff;
+    r->string->data[4] = (i >> 24) & 0xff;
+    r->string->data[5] = (i >> 16) & 0xff;
+    r->string->data[6] = (i >>  8) & 0xff;
+    r->string->data[7] = (i      ) & 0xff;
     r->type = cBytes;
 
     push(exec->stack, r);
 }
 
-void struct_unpackIEEE32(struct tagTExecutor *exec)
+void struct_packIEEE64LE(struct tagTExecutor *exec)
+{
+    Number n = top(exec->stack)->number; pop(exec->stack);
+
+    double x = number_to_double(n);
+    Cell *r = cell_createStringCell(sizeof(x));
+    uint64_t i = *(uint64_t *)&x;
+    r->string->data[7] = (i >> 56) & 0xff;
+    r->string->data[6] = (i >> 48) & 0xff;
+    r->string->data[5] = (i >> 40) & 0xff;
+    r->string->data[4] = (i >> 32) & 0xff;
+    r->string->data[3] = (i >> 24) & 0xff;
+    r->string->data[2] = (i >> 16) & 0xff;
+    r->string->data[1] = (i >>  8) & 0xff;
+    r->string->data[0] = (i      ) & 0xff;
+    r->type = cBytes;
+
+    push(exec->stack, r);
+}
+
+void struct_unpackIEEE32BE(struct tagTExecutor *exec)
 {
     TString *b = top(exec->stack)->string;
 
-    float x;
-    memcpy(&x, b->data, sizeof(x));
+    uint32_t i = (b->data[0] << 24) |
+                 (b->data[1] << 16) |
+                 (b->data[2] <<  8) |
+                 (b->data[3]      );
+    float x = *(float *)&i;
 
     pop(exec->stack);
     push(exec->stack, cell_fromNumber(number_from_float(x)));
 }
 
-void struct_unpackIEEE64(struct tagTExecutor *exec)
+void struct_unpackIEEE32LE(struct tagTExecutor *exec)
 {
     TString *b = top(exec->stack)->string;
 
-    double x;
-    memcpy(&x, b->data, sizeof(x));
+    uint32_t i = (b->data[3] << 24) |
+                 (b->data[2] << 16) |
+                 (b->data[1] <<  8) |
+                 (b->data[0]      );
+    float x = *(float *)&i;
+
+    pop(exec->stack);
+    push(exec->stack, cell_fromNumber(number_from_float(x)));
+}
+
+void struct_unpackIEEE64BE(struct tagTExecutor *exec)
+{
+    TString *b = top(exec->stack)->string;
+
+    uint64_t i = ((uint64_t)b->data[0] << 56) |
+                 ((uint64_t)b->data[1] << 48) |
+                 ((uint64_t)b->data[2] << 40) |
+                 ((uint64_t)b->data[3] << 32) |
+                 ((uint64_t)b->data[4] << 24) |
+                 ((uint64_t)b->data[5] << 16) |
+                 ((uint64_t)b->data[6] <<  8) |
+                 ((uint64_t)b->data[7]      );
+
+    double x = *(double *)&i;
+
+    pop(exec->stack);
+    push(exec->stack, cell_fromNumber(number_from_double(x)));
+}
+
+void struct_unpackIEEE64LE(struct tagTExecutor *exec)
+{
+    TString *b = top(exec->stack)->string;
+
+    uint64_t i = ((uint64_t)b->data[7] << 56) |
+                 ((uint64_t)b->data[6] << 48) |
+                 ((uint64_t)b->data[5] << 40) |
+                 ((uint64_t)b->data[4] << 32) |
+                 ((uint64_t)b->data[3] << 24) |
+                 ((uint64_t)b->data[2] << 16) |
+                 ((uint64_t)b->data[1] <<  8) |
+                 ((uint64_t)b->data[0]      );
+
+    double x = *(double *)&i;
 
     pop(exec->stack);
     push(exec->stack, cell_fromNumber(number_from_double(x)));
--- exec/cnex/lib/struct.h
+++ exec/cnex/lib/struct.h
@@ -3,9 +3,16 @@
 
 struct tagTExecutor;
 
-void struct_packIEEE32(struct tagTExecutor *exec);
-void struct_packIEEE64(struct tagTExecutor *exec);
-void struct_unpackIEEE32(struct tagTExecutor *exec);
-void struct_unpackIEEE64(struct tagTExecutor *exec);
+void struct_packIEEE32BE(struct tagTExecutor *exec);
+void struct_packIEEE32LE(struct tagTExecutor *exec);
+
+void struct_packIEEE64BE(struct tagTExecutor *exec);
+void struct_packIEEE64LE(struct tagTExecutor *exec);
+
+void struct_unpackIEEE32BE(struct tagTExecutor *exec);
+void struct_unpackIEEE32LE(struct tagTExecutor *exec);
+
+void struct_unpackIEEE64BE(struct tagTExecutor *exec);
+void struct_unpackIEEE64LE(struct tagTExecutor *exec);
 
 #endif
